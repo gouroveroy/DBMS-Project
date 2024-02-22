ca0e692f08eb90b468525b66751e27e6b8e8aab5
Md, Sadrul Islam <2105018@ugrad.cse.buet.ac.bd>
2024-02-23 01:16:09 +0600

    Some css by sadrul

--- CRICTOUR/frontend/src/pages/Matches.jsx
+++ CRICTOUR/frontend/src/pages/Matches.jsx
@@ -16,6 +16,19 @@ function getFirstThreeLetters(str) {
   return str.substring(0, 3);
 }
 
+function textFormatDate(dateString) {
+  // Parse the input date string
+  const date = new Date(dateString);
+
+  // Options for formatting the date
+  const options = { year: 'numeric', month: 'long', day: '2-digit' };
+
+  // Format the date using toLocaleDateString()
+  const formattedDate = date.toLocaleDateString('en-GB', options);
+
+  return formattedDate;
+}
+
 function Matches() {
   const { tournament_id } = useParams();
   const [matches, setMatches] = useState([]);
@@ -29,7 +42,7 @@ function Matches() {
       .then(response => response.json())
       .then(data => {
         setMatches(data);
-        if(data.length > 0) {
+        if (data.length > 0) {
           setTournamentName(data[0].tournament_name);
           setHostName(data[0].host);
         }
@@ -50,45 +63,52 @@ function Matches() {
       {matches ? (
         <div className='match-container'>
           {matches.map(match => (
-            <div key={match.match_id} className='match-box'>
-              <div className="match-info">
-                <div className="date-card">
-                  <span>
-                    {getFirstThreeLetters(match.team1_name)} vs {getFirstThreeLetters(match.team2_name)}
-                  </span>
-                </div>
-                <div className="venue-name">
-                  <span>{match.venue_name}</span>
-                </div>
-                <div>
-                  <span>{match.location}</span>
-                </div>
-              </div>
-              <div className="match-between">
-                <div className="team1-match-info">
-                  <div className="team1-name">
-                    <span>{match.team1_name}</span>
-                  </div>
-                  <div className="team1-score">
-                    <span>{match.team1_run}/{match.team1_wicket}</span>
-                  </div>
-                </div>
-                <div className="team2-match-info">
-                  <div className="team2-name">
-                    <span>{match.team2_name}</span>
-                  </div>
-                  <div className="team2-score">
-                    <span>{match.team2_run}/{match.team2_wicket}</span>
-                  </div>
-                </div>
-                <span>
-                  {match.winner_team_name} won the match
+            <div key={match.match_id} className='match-box-outside'>
+              <div className="match-date">
+                <span className='match-date-span'>
+                  {textFormatDate(formatDate(match.match_date))}
                 </span>
               </div>
-              <div className="to-scorecard">
-                <Link to={`/tournaments/${tournament_id}/matches/${match.match_id}`}>
-                  <button className='scoreCardButton'>Scorecard</button>
-                </Link>
+              <div className="match-box">
+                <div className="match-info">
+                  <div className="date-card">
+                    <span>
+                      {getFirstThreeLetters(match.team1_name)} vs {getFirstThreeLetters(match.team2_name)}
+                    </span>
+                  </div>
+                  <div className="venue-name">
+                    <span>{match.venue_name}</span>
+                  </div>
+                  <div>
+                    <span>{match.location}</span>
+                  </div>
+                </div>
+                <div className="match-between">
+                  <div className="team1-match-info">
+                    <div className="team1-name">
+                      <span>{match.team1_name}</span>
+                    </div>
+                    <div className="team1-score">
+                      <span>{match.team1_run}/{match.team1_wicket}</span>
+                    </div>
+                  </div>
+                  <div className="team2-match-info">
+                    <div className="team2-name">
+                      <span>{match.team2_name}</span>
+                    </div>
+                    <div className="team2-score">
+                      <span>{match.team2_run}/{match.team2_wicket}</span>
+                    </div>
+                  </div>
+                  <span>
+                    {match.winner_team_name} won the match
+                  </span>
+                </div>
+                <div className="to-scorecard">
+                  <Link to={`/tournaments/${tournament_id}/matches/${match.match_id}`} style={{ textDecoration: 'none' }}>
+                    <button className='scoreCardButton'>Scorecard</button>
+                  </Link>
+                </div>
               </div>
             </div>
           ))}
